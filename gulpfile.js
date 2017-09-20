2eb652259cb4b11c471e8a42b3df8c1c66060b1c
Janine Hempy <jhempy@gmail.com>
2017-09-20 15:44:12 -0400

    Nice sass workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
 var gulp   = require('gulp');
 var sass   = require('gulp-sass');
 var concat = require('gulp-concat');
+var clean  = require('gulp-clean');
 
-// This is a simple, sample task
-gulp.task('hello', function() {
-  console.log('Hello, there!');
-});
-
-
-// I want to compile 
-// any .scss files
-// in /app/scss
-// and put them
-// into /app/css
-gulp.task('sass', function() {
+gulp.task('sass', [ 'cleanup' ], function() {
   return gulp.src('app/scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('concatenate', function() {
+gulp.task('concatenate', [ 'sass' ], function() {
   return gulp.src('app/css/*.css')
     .pipe(concat('all.css'))
     .pipe(gulp.dest('dist/css/'));
 });
+
+gulp.task('cleanup', function() {
+  return gulp.src(['app/css/*.css', 'dist/css/*.css'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('sassify', ['cleanup', 'sass', 'concatenate']);
+
+gulp.task('watch', function() {
+  gulp.watch('app/scss/*.scss', [ 'sassify' ]);
+});
+
